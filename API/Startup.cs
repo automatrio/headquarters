ab23b16ec8f481fd79e969a6277a8be00a7882c3
Automatrio <daniel.baisi.hadad@gmail.com>
2021-02-24 19:49:35 -0300

    implemented authentication and navbar

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+
 
 namespace API
 {
@@ -29,11 +20,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddCors();
             services.AddControllers();
+            services.AddApplicationServices(_config);
+            services.AddAuthenticationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +33,11 @@ namespace API
 
             app.UseRouting();
 
+		    app.UseCors(options => {
+                options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
